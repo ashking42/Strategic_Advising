e65af16129527e12e9c9e9fd5a5b671060ceb025
Cody York <clyork@bsu.edu>
2018-02-06 15:26:29 -0500

    Update JSONWriterCourse.js

--- json writers/JSONWriterCourse.js
+++ json writers/JSONWriterCourse.js
@@ -3,8 +3,6 @@
  * Split on comma
  */
 var jsonContent = {
-    "courses": [
-        {
             "title": "Computer Science 1",
             "courseNumber": "CS 120",
             "prerequisites": "MATH 111,MATH 112,MATH 132,MATH 136,MATH 161,MATH 165",
@@ -12,8 +10,7 @@ var jsonContent = {
             "restrictions": "",
             "offering": "spring, fall, summer",
             "creditHours": "4"
-        },
-        {
+        ,
           "title": "Applied Calculus 1",
           "courseName": "MATH 161",
           "requisitionNumber": "",
@@ -22,8 +19,6 @@ var jsonContent = {
           "restrictions": "MATH 165",
           "offering": "spring, summer, fall",
           "creditHours": "3"
-        }
-    ]
 };
 
 var fs = require('fs');
