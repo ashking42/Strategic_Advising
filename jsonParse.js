0bae3c7256a6befc779d71ce40f82d2d8e72345b
codyyork <clyork@bsu.edu>
2018-02-08 15:33:07 -0500

    Json prints to webpage with the catagories

--- jsonParse.js
+++ jsonParse.js
@@ -1,33 +1,31 @@
 
 
 $(document).ready(function(){
-    //Trying to get this shortened and useable with require.js
     $("button").click(function(){
        $.getJSON("json/student.json", function(result) {
-           $.each(new  Array(1), function(){
-               $("#listOfClasses").append(result.listOfClasses+ " , " + result.listOfClasses1);
-               $('#studentId').append(result.id + " , " + result.id1);
-               $('#year').append(result.year + " , " + result.year1);
-               $('#yearToGraduate').append(result.yearToGraduate + " , " + result.yearToGraduate1);
-               $('#schedule').append(result.schedule + " , " + result.schedule1);
-               $('#summerClass').append(result.summerClass + " , " + result.summerClass1);
-               $('#major').append(result.major + + " , " + result.major1);
-               $('#transferCredit').append(result.transferCredits + " , " + result.transferCredits1);
-               $('#studentName').append(result.name + " , " + result.name1);
-           });
+           for (var i = 0; i < result.length; i++){
+               $("#listOfClasses").append(result[i].listOfClasses + " ");
+               $('#year').append(result[i].year + " ");
+               $('#yearToGraduate').append(result[i].yearToGraduate + " ");
+               $('#schedule').append(result[i].schedule + " ");
+               $('#summerClass').append(result[i].summerClass + " ");
+               $('#major').append(result[i].major + " ");
+               $('#transferCredit').append(result[i].transferCredits + " ");
+               $('#studentName').append(result[i].name + " ");
+           }
        });
 
         $.getJSON("json/course.json", function(result) {
-            $.each(new  Array(1), function(){
-                $('#name').append(result.title + " , " + result.title1);
-                $('#courseNumber').append(result.courseNumber + " , " + result.courseNumber1);
-                $('#reqNumber').append(result.requisitionNumber + " , " + result.requisitionNumber1);
-                $('#preReq').append(result.prerequisites + " , " + result.prerequisites1);
-                $('#coReq').append(result.corequisite + " , " + result.corequisite1);
-                $('#restrict').append(result.restrictions + " , " + result.restrictions1);
-                $('#offering').append(result.offering + " , " + result.offering);
-                $('#creditHours').append(result.creditHours + " , " + result.creditHours1);
-            });
+            for (var i = 0; i < result.length; i++){
+                $('#name').append(result[i].title + " ");
+                $('#courseNumber').append(result[i].courseNumber + " ");
+                $('#reqNumber').append(result[i].requisitionNumber + " ");
+                $('#preReq').append(result[i].prerequisites + " ");
+                $('#coReq').append(result[i].corequisite + " ");
+                $('#restrict').append(result[i].restrictions + " ");
+                $('#offering').append(result[i].offering + " ");
+                $('#creditHours').append(result[i].creditHours + " ");
+            }
         });
     });
 });
